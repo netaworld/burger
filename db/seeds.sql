0266c2981c0d01bea187d9e69628e0b034736256
Cindy <cchaprak@gmail.com>
2019-11-26 15:52:42 -0500

    created files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheessburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheessburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheessburger', FALSE);
